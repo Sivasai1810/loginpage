f4f251a043f61827f92adf4c757331777fd29155
NALLAMALLI SWAMY SIVA SAI <nsivasai88@gmail.com>
2025-05-23 18:24:11 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,7 @@ require('dotenv').config()
 const mongoosedb=require("mongoose")
 const connectdb = async()=>{
  try{
-     await mongoosedb.connect(process.env.MONGODB_URL,{
-        useNewUrlParser:true,
-        useUnifiedTopology:true ,
-      
-    }) 
+     await mongoosedb.connect(process.env.MONGODB_URL) 
       console.log("successfully connected to the mongodb")
  }catch(error){
  console.log("unable to connect the database")
