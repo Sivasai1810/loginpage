dec245666d8358b2b43082a31153fb5f54bd0742
NALLAMALLI SWAMY SIVA SAI <nsivasai88@gmail.com>
2025-05-23 18:51:11 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,27 +1,11 @@
-// require('dotenv').config()
-// const mongoosedb=require("mongoose")
-// const connectdb = async()=>{
-//  try{
-//      await mongoosedb.connect(process.env.MONGODB_URL) 
-//       console.log("successfully connected to the mongodb")
-//  }catch(error){
-//  console.log("unable to connect the database")
-//  }
-// }
-// module.exports=connectdb
-require('dotenv').config();
-const mongoose = require("mongoose");
-
-const connectdb = async () => {
-  try {
-    console.log("Connecting to MongoDB at:", process.env.MONGODB_URL); 
-    await mongoose.connect(process.env.MONGODB_URL);
-    console.log(" Successfully connected to MongoDB");
-  } catch (error) {
-    console.error(" Unable to connect to the database:", error.message); 
-    console.error(error); 
-  }
-};
-
-module.exports = connectdb;
-
+require('dotenv').config()
+const mongoosedb=require("mongoose")
+const connectdb = async()=>{
+ try{
+     await mongoosedb.connect(process.env.MONGODB_URL) 
+      console.log("successfully connected to the mongodb")
+ }catch(error){
+ console.log("unable to connect the database")
+ }
+}
+module.exports=connectdb
